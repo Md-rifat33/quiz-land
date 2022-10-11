3942dc2e34b965d6949dd27e490d1ab47eecc987
Md-rifat33 <ultimaterifat@yahoo.com>
2022-10-12 01:48:01 +0600

    toast added

--- src/components/QuizDetail/QuizDetail.js
+++ src/components/QuizDetail/QuizDetail.js
@@ -1,17 +1,32 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEye } from '@fortawesome/free-solid-svg-icons'
+import { ToastContainer, toast } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 import React from 'react'
 import QuizOption from '../QuizOption/QuizOption'
 import './QuizDetail.css'
 const QuizDetail = ({ quizs }) => {
-  const { question, options, id } = quizs
-  console.log(quizs)
+  const { question, options, id, correctAnswer } = quizs
+  const notify = () => toast(`${correctAnswer}`)
   return (
-    <div className="quiz">
-      <div>
+    <div className="quiz container">
+      <div className="d-flex justify-content-around align-items-center flex-column flex-md-row">
         <h4 className="text-center">{question}</h4>
+        <FontAwesomeIcon
+          className="eye_icon"
+          onClick={notify}
+          icon={faEye}
+        ></FontAwesomeIcon>
+        <ToastContainer></ToastContainer>
       </div>
-      <div className="quiz_suggestion row ">
+      <div className="quiz_suggestion row">
         {options.map((option, index) => (
-          <QuizOption option={option} id={id} index={index}></QuizOption>
+          <QuizOption
+            option={option}
+            id={id}
+            index={index}
+            correctAnswer={correctAnswer}
+          ></QuizOption>
         ))}
       </div>
     </div>
--- src/components/QuizOption/QuizOption.js
+++ src/components/QuizOption/QuizOption.js
@@ -1,18 +1,31 @@
 import React from 'react'
+import { ToastContainer, toast } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 import './QuizOption.css'
-const QuizOption = ({ option, id, index }) => {
-  console.log(`===index===`, index)
+const QuizOption = ({ option, id, index, correctAnswer }) => {
+  const handleClick = () => {
+    if (correctAnswer === option) {
+      const notify = toast('Well-Done')
+    } else {
+      const notified = toast('This answer is wrong.Please, try another one')
+    }
+  }
   return (
     <div className="quiz_option col-sm-12 col-md-6 col-lg-6 form-check">
       <input
-        className="form-check-input mt-2"
+        className="form-check-input mt-2 ms-5"
         type="radio"
         name="flexRadioDefault"
         id={`${id}_${index}`}
       />
-      <label className="form-check-label" for={`${id}_${index}`}>
+      <label
+        onClick={handleClick}
+        className="form-check-label ms-3"
+        for={`${id}_${index}`}
+      >
         {option}
       </label>
+      <ToastContainer />
     </div>
   )
 }
