a63d5c5ab250396bdd21ba4393fd1bbd254f9a17
Md-rifat33 <ultimaterifat@yahoo.com>
2022-10-12 13:48:56 +0600

    chart completed

--- src/components/Chart/Chart.js
+++ src/components/Chart/Chart.js
@@ -1,6 +1,14 @@
 import React from 'react'
 import { useLoaderData } from 'react-router-dom'
-import { LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip } from 'recharts'
+import {
+  LineChart,
+  Line,
+  CartesianGrid,
+  XAxis,
+  YAxis,
+  Tooltip,
+  ResponsiveContainer,
+} from 'recharts'
 
 const Chart = () => {
   const charts = useLoaderData([])
@@ -8,18 +16,18 @@ const Chart = () => {
   console.log(data)
   return (
     <div className="text-center mt-5 d-flex justify-content-center">
-      <LineChart
-        width={900}
-        height={500}
-        data={data}
-        margin={{ top: 5, right: 20, bottom: 5, left: 0 }}
-      >
-        <Line type="monotone" dataKey="uv" stroke="#8884d8" />
-        <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
-        <XAxis dataKey="name" />
-        <YAxis dataKey="total" />
-        <Tooltip></Tooltip>
-      </LineChart>
+      <ResponsiveContainer width={'100%'} height={500}>
+        <LineChart
+          data={data}
+          margin={{ top: 5, right: 20, bottom: 5, left: 0 }}
+        >
+          <Line type="monotone" dataKey="total" stroke="#8884d8" />
+          <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
+          <XAxis dataKey="name" />
+          <YAxis dataKey="total" />
+          <Tooltip></Tooltip>
+        </LineChart>
+      </ResponsiveContainer>
     </div>
   )
 }
